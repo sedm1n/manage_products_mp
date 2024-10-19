c36117567448cdbb631571a609eba0b3dfe4b9df
sedm1n <4310481@gmail.com>
2024-10-19 15:48:09 +0300

    add logger settings on logger.py

---
+++ app/logger.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from app.backend.config import cfg
+import logging
+from pythonjsonlogger import jsonlogger
+
+logger = logging.getLogger()
+logHandler = logging.StreamHandler()
+
+class CustomJsonFormatter(jsonlogger.JsonFormatter):
+      def add_fields(self, log_record, record, message_dict):
+            super(CustomJsonFormatter, self).add_fields(log_record,record, message_dict)
+            if not log_record.get('timestamp'):
+                  now = datetime.datetime.utcnow().strftime("%Y-%m-%dT %H:%M:%S")
+                  log_record['timestamp'] = now
+            if log_record.get('level'):
+                  log_record['level'] = log_record['level'].upper()
+            else:
+                  log_record['level'] = record.levelname
+
+formatter = CustomJsonFormatter(
+      "%(timestamp)s %(level)s %(message)s %(module)s %(funcName)s"
+)
+
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+logger.setLevel(cfg.LOG_LEVEL)`
